d8c20a02f0af9edfb7697ab0c53b051c976a626e
Noora Isoaho <isoaho.noora@gmail.com>
2023-08-21 11:26:16 +0200

    fix library steps

--- writedockerfile.py
+++ writedockerfile.py
@@ -5,28 +5,25 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dockerfile', type=str, default="")
-    parser.add_argument('--image-name', type=str, default="")
     parser.add_argument('--docker-tag', type=str, default="")
     parser.add_argument('--repository', type=str, default="")
     return parser.parse_args()
 
 def check_input():
-
-    if os.path.isfile('/valohai/inputs/dockerfile/Dockerfile'):
+    if os.path.isfile('/valohai/inputs/dockerfileinput/Dockerfile'):
         return True
     else:
         return False
     
 def main():
-
+    # Check if the Dockerfile was provided as an input and copy it to the working directory.
     if check_input():
         print("Dockerfile provided as an input.")
-        shutil.copy('/valohai/inputs/dockerfile/Dockerfile', '/valohai/repository/Dockerfile')
+        shutil.copy('/valohai/inputs/dockerfileinput/Dockerfile', '/valohai/repository/Dockerfile')
     else:
-        # Reading Dockerfile contents from the parameter
+        # Read the contents from the dockerfile parameter
+        # and write them into Dockerfile
         args = parse_args()
- 
-        #contents = valohai.parameters('dockerfile').value
 
         if args.dockerfile:
             print("Dockerfile contents provided as a parameter.")
@@ -36,4 +33,5 @@ def main():
         else:
             print("Please provide the Dockerfile contents as a parameter or the actual file as an input.") 
 
-main()
+if __name__ == '__main__':
+    main()
